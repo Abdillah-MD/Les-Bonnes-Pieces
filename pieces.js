b1d4f623a2acc0d1ce67d729466200e7c4d43876
Abdillah-MD <ambdi6@gmail.com>
2023-11-23 10:58:36 +0100

    Chapitre fonct° (splice, sort, filter) git status pwd man dquote q

--- pieces.js
+++ pieces.js
@@ -34,7 +34,7 @@ for (let i = 0; i < pieces.length; i++) {
 
 }
 
-// Permet d'écouter le click btn filtre et réordoner la liste 
+// Permet d'écouter le click btn filtre et réordoner la liste  APRENDRE LA FONCTI° ******SORT*****
 const boutonTrier = document.querySelector(".btn-trier")
 boutonTrier.addEventListener("click", () => {
     const piecesOrdonnees = Array.from(pieces)
@@ -44,7 +44,7 @@ boutonTrier.addEventListener("click", () => {
     console.log(piecesOrdonnees)
 })
 
-// Permet d'écouter le click et filtrer le résultat
+// Permet d'écouter le click et filtrer le résultat  APRENDRE LA FONCT° *********FILTER********
 const boutonFiltre = document.querySelector(".btn-filtrer")
 boutonFiltre.addEventListener("click", () => {
     const piecesFiltres = pieces.filter(function(piece) {
@@ -53,7 +53,7 @@ boutonFiltre.addEventListener("click", () => {
     console.log(piecesFiltres)
 })
 
-// Ecouter le click puis cacher produits sans description 
+// Ecouter le click puis cacher produits sans description APRENDRE LA FONCT° *********FILTER********
 const boutonDescription = document.querySelector(".btn-description")
 boutonDescription.addEventListener("click", () => {
     const piecesDescription = pieces.filter(function(piece) {
@@ -62,7 +62,7 @@ boutonDescription.addEventListener("click", () => {
     console.log(piecesDescription)
 })
 
-// Ecouter le click puis trier dans l'ordre décroissant 
+// Ecouter le click puis trier dans l'ordre décroissant APRENDRE LA FONCTI° ******SORT*****
 const boutonDecroissant = document.querySelector(".btn-prix_Decroissant")
 boutonDecroissant.addEventListener("click", () => {
     const piecesDecroissant = Array.from(pieces)
@@ -71,3 +71,55 @@ boutonDecroissant.addEventListener("click", () => {
     })
     console.log(piecesDecroissant)
 })
+
+// Maper le tableau le tableau pour extraire liste "nom produit"
+const noms = pieces.map(piece => piece.nom)
+for (let i = pieces.length -1; i >= 0; i--) {
+    if (pieces[i].prix > 35) {
+        noms.splice(i, 1)
+    }
+}
+console.log(noms)
+
+// Maper le tableau pour avoir liste "prix produit"
+const nomsPrix = pieces.map(piece => piece.prix)
+for (let i = pieces.length -1; i >= 0; i--) {
+    if (pieces[i].prix > 35) {
+        nomsPrix.splice(i, 1)
+    }
+}
+console.log(nomsPrix)
+
+// Création de la liste 
+const abordablesElements = document.createElement("ul")
+for (let i=0; i < noms.length; i++) {
+    const nomElement = document.createElement("li")
+    nomElement.innerText = `${noms[i]} - (${nomsPrix[i]} €)`
+    abordablesElements.appendChild(nomElement)
+}
+document.querySelector(".abordables")
+    .appendChild(abordablesElements)
+
+
+// Creation liste description 
+const abordablesDescription = document.createElement("ul")
+const nomsDescription = pieces.map(piece => piece.description)
+// Supprimer les description qui ne nous interesse pas 
+for (let i = noms.length -1; i >= 0; i--) {
+    if (pieces[i].prix > 35) {
+        nomsDescription.splice(i, 1)
+    }
+}
+// Injecter les description dans l'ordre voulu
+for (let i = 0; i < noms.length; i++) {
+    const nomElement = document.createElement("li")
+    nomElement.innerText = nomsDescription[i] ?? "(Pas de description)"
+    abordablesDescription.appendChild(nomElement)
+}
+console.log(nomsDescription)
+document.querySelector(".description")
+    .appendChild(abordablesDescription)
+
+
+/// Chapitre innertHTML
+document.querySelector(".fiches").innerHTML = ''
